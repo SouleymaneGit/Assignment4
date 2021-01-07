72644e9667e7bdce53890b3f6a1caac3d7a56557
Thierno Souleymane Diallo <thiernosouleymane24@gmail.com>
2021-01-07 17:52:49 -0500

    putting together the contains of all html files: bonus, part3, hover, and twoButtons into the index file

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 
-
+//script for the input form and the even hendling
 const tobeExecuted = () =>{
 
     const userpassword = document.getElementById("password").value;
@@ -15,6 +15,33 @@ const tobeExecuted = () =>{
     }
    
 }
-// testing
 
 document.getElementById("submit").addEventListener('click', tobeExecuted)
+
+// script for the hoving
+
+function Alert(e){
+    window.alert("Hey, I told you not to hover over me");
+};
+
+//script for the bonus part
+
+function multiplyBy(){
+    let pie = Math.PI;;
+    value = document.getElementById("radius").value;
+    result  = (4.0/3.0)*pie*(value*value*value);
+
+      
+    n = result.toFixed(2);
+    window.alert("volume rounded for 2 decimal places =" +n);
+}
+
+// script for the Two buttons arguing 
+
+function imRight() {
+    document.querySelector("p").innerHTML = "Im Right"
+}
+
+function noImRight() {
+    document.querySelector("p").innerHTML = "No, Im Right"
+}
